3c372193a590b72a8f0bbe16b9f5878fd691f811
Rodrigo Godoy <ro.godoyr@duocuc.cl>
2026-05-25 14:13:47 -0400

    no deben ir

--- libro-service/src/main/java/com/lexicon/libro/exception/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package com.lexicon.libro.exception;
-import org.springframework.http.HttpStatus;
-
-public class AuthenticationException {
-     public AuthenticationException(String message) {
-        super("AUTHENTICATION_ERROR", message, HttpStatus.UNAUTHORIZED);
-    }
-}
--- libro-service/src/main/java/com/lexicon/libro/exception/AuthorizationException.java
+++
@@ -1,8 +0,0 @@
-package com.lexicon.libro.exception;
-import org.springframework.http.HttpStatus;
-
-public class AuthorizationException {
-    public AuthorizationException(String message) {
-        super("AUTHORIZATION_ERROR", message, HttpStatus.FORBIDDEN);
-    }
-}
